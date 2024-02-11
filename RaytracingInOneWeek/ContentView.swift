3874287f2cd8b6c715d7093de8a61022f5734b35
Jae <jaechoi.dev@gmail.com>
2024-02-10 16:07:31 -0800

    Initial Commit

---
+++ RaytracingInOneWeek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RaytracingInOneWeek
+//
+//  Created by Jae Choi on 2024-02-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RaytracingInOneWeek/RaytracingInOneWeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaytracingInOneWeekApp.swift
+//  RaytracingInOneWeek
+//
+//  Created by Jae Choi on 2024-02-10.
+//
+
+import SwiftUI
+
+@main
+struct RaytracingInOneWeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
